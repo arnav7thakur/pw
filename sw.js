264147717f516a86d7d81696714729ee306aee54
arnav7thakur <arnav19thakur@gmail.com>
2025-03-24 11:07:03 +0530

    update

--- sw.js
+++ sw.js
@@ -1,5 +1,11 @@
-self.addEventListener('push', () => {
-    self.registration.showNotification('Did you get this?', options);
-  });
+self.addEventListener('push', (event) => {
+  const options = {
+      body: 'This is a push notification!',
+      icon: '/icon.png', // Optional: Path to an icon
+      vibrate: [200, 100, 200], // Optional: Vibration pattern
+  };
 
-
+  event.waitUntil(
+      self.registration.showNotification('Did you get this?', options)
+  );
+});
