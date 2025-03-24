e525aac19a60b64724e899db5a61a91006968b9f
arnav7thakur <arnav19thakur@gmail.com>
2025-03-24 11:10:08 +0530

    works?

--- sw.js
+++ sw.js
@@ -1,11 +1,27 @@
-self.addEventListener('push', (event) => {
-  const options = {
-      body: 'This is a push notification!',
-      icon: '/icon.png', // Optional: Path to an icon
-      vibrate: [200, 100, 200], // Optional: Vibration pattern
+self.addEventListener('push', function(e) {
+  var options = {
+      body: 'This notification was generated from a push!',
+      icon: 'images/example.png',
+      vibrate: [100, 50, 100],
+      data: {
+          dateOfArrival: Date.now(),
+          primaryKey: '2'
+      },
+      actions: [
+          {
+              action: 'explore',
+              title: 'Explore this new world',
+              icon: 'images/checkmark.png'
+          },
+          {
+              action: 'close',
+              title: 'Close',
+              icon: 'images/xmark.png'
+          }
+      ]
   };
 
-  event.waitUntil(
-      self.registration.showNotification('Did you get this?', options)
+  e.waitUntil(
+      self.registration.showNotification('Hello world!', options)
   );
 });
