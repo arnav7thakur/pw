a0f3ca3358f28e76de752c217799311318cd3bef
arnav7thakur <arnav19thakur@gmail.com>
2025-03-24 10:43:21 +0530

    fix

--- script.js
+++ script.js
@@ -10,4 +10,20 @@ async function subscribe() {
         applicationServerKey: 'BA9W0Y6_0zPUQWo-adS1tKtHC3BGqtdMYI-I09PhPisQ-F8DXf2DT3b4qFwdGrqg43yOrTnNTuQcHhAe1qfQr4w'
     })
     console.log(JSON.stringify(push));
+<<<<<<< HEAD
+=======
+}
+addEventListener('load', async () => {
+    let sw = await navigator.serviceWorker.register('./sw.js')
+    console.log(sw);
+})
+
+async function subscribe() {
+    let sw = await navigator.serviceWorker.ready;
+    let push = await sw.pushManager.subscribe({
+        userVisibleOnly: true, 
+        applicationServerKey: 'BA9W0Y6_0zPUQWo-adS1tKtHC3BGqtdMYI-I09PhPisQ-F8DXf2DT3b4qFwdGrqg43yOrTnNTuQcHhAe1qfQr4w'
+    })
+    console.log(JSON.stringify(push));
+>>>>>>> a679f76 (fix)
 }
